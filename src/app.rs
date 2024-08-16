6edd742a54f10abd770932630a2cd92fc2625299
Kameron Scott <kameron-scott@outlook.com>
2024-08-16 00:35:11 -0500

    adds `WindowState` for wgpu, and begins incorporation with `App`

--- src/app.rs
+++ src/app.rs
@@ -1,23 +1,39 @@
 use winit::application::ApplicationHandler;
 use winit::event::WindowEvent;
 use winit::event_loop::ActiveEventLoop;
-use winit::window::{Window, WindowId};
+use winit::window::{ Window, WindowId };
+
+mod windowstate;
+use windowstate::WindowState;
 
 #[derive(Default)]
-pub struct App {
+pub struct App<'a> {
     window: Option<Window>,
+    state: Option<WindowState<'a>>,
 }
 
-impl ApplicationHandler for App {
+impl<'a> App<'a> {
+    fn window(&self) -> &Window {
+        self.window.as_ref().expect("Window is not initialized")
+    }
+}
+
+impl<'a> ApplicationHandler for App<'a> {
     fn resumed(&mut self, event_loop: &ActiveEventLoop) {
         self.window = Some(
             event_loop
                 .create_window(Window::default_attributes().with_title("Auto-TAS"))
                 .unwrap(),
         );
+        // if self.state.is_none() {
+        //     let window = self.window.as_ref().unwrap();
+        //     let new_state = tokio::runtime::Handle::current().block_on(WindowState::new(window));
+        //     self.state = Some(new_state);
+        //     println!("state built")
+        // }
     }
 
-    fn window_event(&mut self, event_loop: &ActiveEventLoop, id: WindowId, event: WindowEvent) {
+    fn window_event(&mut self, event_loop: &ActiveEventLoop, _id: WindowId, event: WindowEvent) {
         match event {
             WindowEvent::CloseRequested => {
                 println!("The close button was pressed; stopping");
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
 mod app;
-
 use app::App;
+
 use winit::event_loop::{ControlFlow, EventLoop};
 
 fn main() {
-    let event_loop = EventLoop::new().unwrap();
-    event_loop.set_control_flow(ControlFlow::Poll);
+    env_logger::init();
+
+    let event_loop: EventLoop<()> = EventLoop::new().unwrap();
+
     event_loop.set_control_flow(ControlFlow::Wait);
-    let mut app = App::default();
-    let _ = event_loop.run_app(&mut app);
+    let mut app: App = App::default();
+    let _ = event_loop.run_app(&mut app); // [NOTE] `let _ = ` is not necessary
 }
